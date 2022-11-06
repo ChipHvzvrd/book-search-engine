6f44c2eefa04d71b70289755f621da02eab47c2c
chiphvzvrd <christianipena@yahoo.com>
2022-11-06 00:50:40 -0500

    typeDefs defined

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { User } = require('../models');
+
+
+const resolvers = {
+    Query: {
+      helloWorld: () => {
+        return 'Hello world!';
+      }
+    }
+  };
+  
+  module.exports = resolvers;
